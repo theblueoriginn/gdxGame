c721f29a6a407c6e0158710a97fe41e6afc83426
tuna <tunaylmzreel@gmail.com>
2026-02-10 21:07:36 +0300

    Adding delta movement  and cursor.png

--- core/src/main/java/org/tunaylmz/Main.java
+++ core/src/main/java/org/tunaylmz/Main.java
@@ -4,10 +4,13 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Cursor;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -28,6 +31,13 @@ public class Main extends Game {
     Array<Sprite> enemySprites;
     Texture enemyTexture;
 
+    float enemyTimer;
+//collision detection
+    Rectangle charRectangle;
+    Rectangle enemyRectangle;
+    private boolean destination = false;
+    private Vector2 destPos;
+
 
     @Override
     public void create() {
@@ -36,27 +46,40 @@ public class Main extends Game {
         batch = new SpriteBatch();
         viewport = new FitViewport(width, height);
         charSprite = new Sprite(charTexture);
+
+        charSprite.setPosition(width/2,height/2);
         charSprite.setSize(128, 128);
         touchPos = new Vector2();
 
         enemySprites = new Array<>();
 
-        createEnemies();
+        charRectangle = new Rectangle();
+        enemyRectangle = new Rectangle();
+
+        createCursor();
+
 
     }
+    public  void createCursor (){
+        Pixmap pixmap = new Pixmap(Gdx.files.internal("cursor.png"));
+        Cursor cursor= Gdx.graphics.newCursor(pixmap,6,6);
+        pixmap.dispose();
+        Gdx.graphics.setCursor(cursor);
+    }
     public void createEnemies(){
-        float dropWidth = 128;
-        float dropHeight = 128;
+        float enemyWidth = 8;
+        float enemyHeight = 8;
         float worldWidth = viewport.getWorldWidth();
         float worldHeight = viewport.getWorldHeight();
 
         enemyTexture = new Texture("bucket.png");
         // create the drop sprite
         Sprite enemySprite = new Sprite(enemyTexture);
-        enemySprite.setSize(dropWidth, dropHeight);
-        enemySprite.setX(0);
+        enemySprite.setSize(enemyWidth, enemyHeight);
+        enemySprite.setX(MathUtils.random(0f,worldWidth - enemyWidth)  );
         enemySprite.setY(worldHeight);
         enemySprites.add(enemySprite);
+
     }
 
     public void pause(){
@@ -74,6 +97,8 @@ public class Main extends Game {
         for (Sprite enemySprite : enemySprites) {
             enemySprite.draw(batch);
         }
+
+
 
         batch.end();
     }
@@ -95,11 +120,26 @@ public class Main extends Game {
         else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             charSprite.translateX(-speed * delta); // move the bucket left
         }
-        if (Gdx.input.isTouched()) { // If the user has clicked or tapped the screen
-            touchPos = new Vector2(Gdx.input.getX(), Gdx.input.getY());
-            viewport.unproject(touchPos);
-            charSprite.setCenterX(touchPos.x);
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            charSprite.translateY(speed * delta); // Move the bucket UP
         }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            charSprite.translateY(-speed * delta); // Move the bucket DOWN
+        }
+
+        //mouse controllers.
+
+        touchPos = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+        touchPos = viewport.unproject(touchPos);
+
+        Vector2 charPos = new Vector2(charSprite.getX(),charSprite.getY());
+        if (Gdx.input.isTouched()   &&  !charPos.equals(touchPos) ) { // If the user has clicked and is not in same pos as touchPos.
+                destination = true;
+                destPos = touchPos;
+
+            }
+
+
     }
 
     private void logic() {
@@ -110,6 +150,52 @@ public class Main extends Game {
         float charHeight = charSprite.getHeight();
 
         charSprite.setX(MathUtils.clamp(charSprite.getX(), 0, worldWidth-charWidth));
+
+        float delta = Gdx.graphics.getDeltaTime();
+        charRectangle.set(charSprite.getX() ,charSprite.getY(),charWidth,charHeight);
+
+        //destroy if out of bounds.
+        for (int i = enemySprites.size - 1; i >= 0; i--) {
+            Sprite enemySprite = enemySprites.get(i); // Get the sprite from the list
+            float enemyWidth = enemySprite.getWidth();
+            float enemyHeight = enemySprite.getHeight();
+
+            enemySprite.translateY(-90f * delta);
+
+            enemyRectangle.set(enemySprite.getX() , enemySprite.getY(),enemyWidth,enemyHeight);
+
+
+            // if the top of the drop goes below the bottom of the view, remove it
+            if (enemySprite.getY() < -enemyHeight) enemySprites.removeIndex(i);
+            else if(enemyRectangle.overlaps(charRectangle) ){
+                    enemySprites.removeIndex(i);
+            }
+        }
+
+
+        enemyTimer += delta;
+        if(enemyTimer > 1.f){
+            createEnemies();
+            enemyTimer = 0.f;
+        }
+
+        if(destination && destPos != null){
+            Vector2 charPos = new Vector2(charSprite.getX(),charSprite.getY());
+            viewport.unproject(charPos);
+            if(destPos != charPos){
+
+                charSprite.translate( (destPos.x  - charSprite.getX())* 2f* delta ,  (destPos.y  - charSprite.getY()) * 2f * delta     );
+
+            }
+            else{
+                destination  = false;
+                destPos = null;
+            }
+        }
+
+
+
+
     }
 
     @Override
